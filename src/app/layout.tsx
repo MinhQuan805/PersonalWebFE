744018cb5b6ef265247fe045b35b955d3ca745d2
Minh Quan <minhquanvo2006@gmail.com>
2025-08-20 13:26:11 +0700

    Sửa phần cảm nhận khách hàng

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Providers>
-          <AppInitializer />
           {children}
         </Providers>
       </body>
