79145184039c6cb2a6eea9ef13a3632d6ad19886
Minh Quan <minhquanvo2006@gmail.com>
2025-08-20 23:04:17 +0700

    Sửa thêm data

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import ClientLayout from '@/components/layout/ClientLayout'
+import ClientLayout from '@/components/ClientLayout'
 
 export default function ClientRootLayout({ children }: { children: React.ReactNode }) {
   return <ClientLayout>{children}</ClientLayout>
