0837512b8b51ef36b80c413675ecc0e6db8580a4
Minh Quan <minhquanvo2006@gmail.com>
2025-08-31 09:22:31 +0700

    Quannotes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import { SWRConfigProvider } from '@/components/SWRConfigProvider'
 import "@/types/i18n";
+import type { Metadata } from 'next';
+import personal from '@/data/personal.json';
+
+export const metadata: Metadata = {
+  title: "Quan Notes",
+  icons: {
+    icon: personal.about.logo,
+  },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
