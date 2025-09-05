b4d7342492b206068b48f2cf88be6aaac7b02d79
Minh Quan <minhquanvo2006@gmail.com>
2025-09-05 13:52:09 +0700

    Quan Notes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
-const { i18n } = require('./next-i18next.config');
 const nextConfig: NextConfig = {
   transpilePackages: ['@ant-design/v5-patch-for-react-19'],
   experimental: {
     optimizePackageImports: ['antd', '@ant-design/icons'],
   },
-  i18n,
   // webpack: (config) => {
   //   config.resolve.fallback = {
   //     ...config.resolve.fallback,
