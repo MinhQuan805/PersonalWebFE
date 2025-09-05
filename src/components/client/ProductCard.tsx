d1f18c3a19dc1facc1bce6131e48f1794788c992
Minh Quan <minhquanvo2006@gmail.com>
2025-09-05 18:09:51 +0700

    Quan Notes

--- src/components/client/ProductCard.tsx
+++ src/components/client/ProductCard.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import { Card, Typography, Button } from 'antd';
 import "@/styles/client/main.css"
 import { ProductType } from '@/lib/models/product.model';
+import { sendGAEvent } from '@next/third-parties/google';
+import { FaGithub } from 'react-icons/fa';
+import personal from '@/data/personal.json';
 const { Paragraph } = Typography;
 
 type Props = {
   product: ProductType;
   style: { [key: string]: string };
+};
+
+const handleLearnMore = (productTitle: string, NavLink: string) => {
+  sendGAEvent('event', 'product_learn_more', { product: productTitle });
+  window.open(`${NavLink}`, "_blank");
 };
 
 const ProductCard: React.FC<Props> = ({ product, style }) => {
@@ -25,9 +33,28 @@ const ProductCard: React.FC<Props> = ({ product, style }) => {
           {product.shortDescription}
         </Paragraph>
         <div className={style.action}>
-          <Button className={style.productButton} style={{ background: '#eaf3fa', color: '#222' }}>Tìm hiểu</Button>
-          <a href={product.github} target="_blank" rel="noopener noreferrer"><img src="/image/logo/github.png" alt="GitHub" /></a>
-          <Button className={style.productButton} style={{ background: '#eaf3fa', color: '#222' }}>Feedback</Button>    
+          <Button className={style.productButton}
+            onClick={() => handleLearnMore(product.title, product.github)}
+            style={{ background: '#eaf3fa', color: '#222' }}
+            icon={<FaGithub />}
+          > Tìm hiểu
+          </Button>
+          <a
+            href={`https://mail.google.com/mail/?view=cm&fs=1&to=${personal.about.info.email}&su=${encodeURIComponent(
+              `Feedback cho sản phẩm ${product.title}`
+            )}&body=${encodeURIComponent(
+              `Xin chào Quân,\nTôi muốn góp ý về sản phẩm ${product.title}...`
+            )}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              className={style.productButton}
+              style={{ background: '#eaf3fa', color: '#222' }}
+            >
+              Feedback
+            </Button>
+          </a>
         </div>
       </div>
     </Card>
